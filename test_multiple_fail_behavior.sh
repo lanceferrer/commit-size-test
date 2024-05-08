66a372d470c5f7983d884efe7c4772dcae036aac
lanceferrer <lintzferr@gmail.com>
2024-05-08 11:00:27 -0400

    add another test

---
+++ test_multiple_fail_behavior.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dd if=/dev/zero of=_101MB.txt bs=1000000 count=101
+dd if=/dev/zero of=_100MB_5.txt bs=1000000 count=100
+dd if=/dev/zero of=_100MB_4.txt bs=1000000 count=100
+dd if=/dev/zero of=_100MB_3.txt bs=1000000 count=100
+dd if=/dev/zero of=_100MB_2.txt bs=1000000 count=100
+dd if=/dev/zero of=_100MB_1.txt bs=1000000 count=100
